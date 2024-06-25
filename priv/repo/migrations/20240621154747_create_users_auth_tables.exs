ddeda1f41aec8f3acb9a4e511a8f8951747872ba
Ariadne Engelbrecht <ariadneengelbrecht@dionysos.local>
2024-06-25 06:51:05 +0200

    Setup user auth for registration.

---
+++ priv/repo/migrations/20240621154747_create_users_auth_tables.exs
@@ -0,0 +1,35 @@
+defmodule TaskMaster.Repo.Migrations.CreateUsersAuthTables do
+  use Ecto.Migration
+
+  def change do
+    execute "CREATE EXTENSION IF NOT EXISTS citext", ""
+
+    create table(:users, primary_key: false) do
+      add :id, :uuid, primary_key: true, null: false
+      add :first_name, :string, null: false
+      add :last_name, :string, null: false
+      add :nick_name, :string, null: true
+      add :email, :citext, null: false
+      add :roles, {:array, :string}, null: false, default: []
+      add :hashed_password, :string, null: false
+      add :confirmed_at, :naive_datetime
+
+      timestamps()
+    end
+
+    create unique_index(:users, [:email])
+
+    create table(:users_tokens, primary_key: false) do
+      add :id, :uuid, primary_key: true, null: false
+      add :user_id, references(:users, on_delete: :delete_all, type: :uuid), null: false
+      add :token, :binary, null: false
+      add :context, :string, null: false
+      add :sent_to, :string
+
+      timestamps(updated_at: false)
+    end
+
+    create index(:users_tokens, [:user_id])
+    create unique_index(:users_tokens, [:context, :token])
+  end
+end
