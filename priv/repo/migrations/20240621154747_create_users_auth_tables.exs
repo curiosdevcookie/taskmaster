4eb6740199b2b9cd6b8de12633909597cfa89c4f
Ariadne Engelbrecht <ariadneengelbrecht@dionysos.local>
2024-06-29 14:43:34 +0200

    Modify users table and add avatars table.

--- priv/repo/migrations/20240621154747_create_users_auth_tables.exs
+++ priv/repo/migrations/20240621154747_create_users_auth_tables.exs
@@ -8,16 +8,28 @@ defmodule TaskMaster.Repo.Migrations.CreateUsersAuthTables do
       add :id, :uuid, primary_key: true, null: false
       add :first_name, :string, null: false
       add :last_name, :string, null: false
-      add :nick_name, :string, null: true
+      add :nick_name, :citext
       add :email, :citext, null: false
-      add :roles, {:array, :string}, null: false, default: []
+      add :roles, {:array, :string}, null: false, default: ["editor"]
       add :hashed_password, :string, null: false
       add :confirmed_at, :naive_datetime
+      add :last_login_at, :naive_datetime
 
       timestamps()
     end
 
     create unique_index(:users, [:email])
+    create unique_index(:users, [:nick_name])
+
+    create table(:avatars, primary_key: false) do
+      add :id, :uuid, primary_key: true, null: false
+      add :path, :string, null: false
+      add :user_id, references(:users, type: :uuid, on_delete: :delete_all), null: false
+
+      timestamps()
+    end
+
+    create index(:avatars, [:user_id])
 
     create table(:users_tokens, primary_key: false) do
       add :id, :uuid, primary_key: true, null: false
