e6d39eb6fe46bc4218bf361a21be2a32c7ee75c7
Ariadne Engelbrecht <ariadneengelbrecht@dionysos.local>
2024-10-28 06:50:20 +0100

    Add port back to config.

--- config/runtime.exs
+++ config/runtime.exs
@@ -49,13 +49,14 @@ if config_env() == :prod do
       You can generate one by calling: mix phx.gen.secret
       """
 
-  port = String.to_integer(System.get_env("PORT") || "4000")
   host = System.get_env("PHX_HOST") || "taskmaster.studio"
+  port = String.to_integer(System.get_env("PORT") || "4000")
 
   config :task_master, TaskMasterWeb.Endpoint,
-    url: [host: host, scheme: "https"],
+    url: [host: host, port: 443, scheme: "https"],
     http: [
-      ip: {0, 0, 0, 0, 0, 0, 0, 0}
+      ip: {0, 0, 0, 0, 0, 0, 0, 0},
+      port: port
     ],
     secret_key_base: secret_key_base
 
